930a49e3db842f8cef849b9aa813c30633bc66e5
Gabe Berke-Williams <gabebw@gabebw.com>
2019-08-25 12:42:18 -0700

    Make some of the Result handling nicer

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,37 @@ use dirs;
 use glob::glob;
 use lz4::block;
 use std::fs::File;
-use std::io::{self, Read, Seek, SeekFrom};
-use std::path::{Path};
-use std::result;
-use std::str::{self, Utf8Error};
+use std::io::{self, ErrorKind, Read, Seek, SeekFrom};
+use std::path::{Path, PathBuf};
+use std::str;
 
 fn main() {
     let mut path = format!("{}", dirs::home_dir().unwrap().display());
     path.push_str("/Library/Application Support/Firefox/Profiles");
     path.push_str("/*default*/sessionstore-backups/recovery.jsonlz4");
     let path = "recovery.jsonlz4";
+
     for result in glob(&path).unwrap() {
-        // result is Result<std::path::PathBuf, glob::GlobError>
-        if let Ok(item) = result {
-            match decompress(&Path::new(&item)) {
-                Ok(s) => println!("{}", convert_to_string(&s).unwrap()),
-                Err(e) => eprintln!("Error: {}", e)
-            }
-        } else {
-            eprintln!("Glob error");
+        let item = result.unwrap();
+        match decompressed_contents(item) {
+            Ok(s) => println!("{}", s),
+            Err(e) => eprintln!("{}", e)
         }
     }
 }
 
-fn convert_to_string<'a>(blocks: &'a Vec<u8>) -> result::Result<&'a str, Utf8Error> {
-    str::from_utf8(&blocks[..])
+fn decompressed_contents(item: PathBuf) -> io::Result<String> {
+    let s = decompress(&Path::new(&item))?;
+    Ok(convert_to_string(&s)?.to_string())
+}
+
+// Re-wrap the `Utf8Error` in `str::from_utf8` in an `io::Error` so we can always return an
+// `io::Result` in `decompressed_contents`
+fn convert_to_string<'a>(blocks: &'a Vec<u8>) -> io::Result<&'a str> {
+    match str::from_utf8(&blocks[..]) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(io::Error::new(ErrorKind::Other, e))
+    }
 }
 
 fn decompress(source: &Path) -> io::Result<Vec<u8>> {
